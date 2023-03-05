93bba69dd7b5874044476932a1dcb80c307dee29
Arif5577 <arifalamm33@gmail.com>
2023-03-05 07:13:47 +0600

    Teleport Component

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div class="container py-5">
-    <div class="w-1/2 mx-auto">
-      <div class="flex items-center gap-5 mt-5">
-        <button
-          @click="activeTab = 'TabA'"
-          class="px-6 py-2 bg-slate-900 text-white text-sm"
-        >
-          Tab A
-        </button>
-        <button
-          @click="activeTab = 'TabB'"
-          class="px-6 py-2 bg-slate-900 text-white text-sm"
-        >
-          Tab B
-        </button>
-      </div>
-      <keep-alive>
-        <component :is="activeTab" />
-      </keep-alive>
-    </div>
+    <teleport to="#newApp">
+      <Portal />
+    </teleport>
   </div>
 </template>
 
 <script>
 import TabA from "./components/TabA.vue";
 import TabB from "./components/TabB.vue";
+import Portal from "./components/Portal.vue";
 export default {
   components: {
     TabA,
     TabB,
+    Portal,
   },
 
   data() {
---
+++ vue-app/src/components/Portal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="py-5 container">
+    <h2>TELEPORT COMPONENT</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Portal",
+};
+</script>
+
+<style lang="scss" scoped></style>
